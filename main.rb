0672d2f852b99d18ef9083e47fad5df66bd988e2
AndreiPopovici <pasandrei98@gmail.com>
2022-07-30 21:17:07 +0300

    Save data to csv

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ require './models/advertisement'
 require 'pry'
 require 'pry-nav'
 
-base_url = "https://www.storia.ro/_next/data/pgVOGyuLbnviUF3fMMbc8/ro/cautare/inchiriere/apartament/bihor/oradea.json"
+base_url = "https://www.storia.ro/_next/data/Aw6VPVhS0t4d2Tlrs1qax/ro/cautare/inchiriere/apartament/bihor/oradea.json"
 page = 1
 total_ads = 0
 total_price = 0
@@ -35,18 +35,34 @@ loop do
 
   total_ads += ads.count
   page += 1
+  break
 end
 
-puts "Total number of ads: #{total_ads}"
-puts "Average price: #{total_price / total_ads}"
+average_price = total_price / total_ads
+
 ads_grouped = ads_list.group_by(&:rooms_number)
-puts "Average price one room: #{ads_grouped[1].sum{|ad| ad.get_price_in_euro} / ads_grouped[1].count}"
-puts "Average price two rooms: #{ads_grouped[2].sum{|ad| ad.get_price_in_euro} / ads_grouped[2].count}"
-puts "Average price three rooms: #{ads_grouped[3].sum{|ad| ad.get_price_in_euro} / ads_grouped[3].count}"
 ads_with_four_or_more_rooms = []
 ads_grouped.each do |rooms_number, ads|
   ads_with_four_or_more_rooms += ads if rooms_number >= 4
 end
-puts "Average price four+ rooms: #{ads_with_four_or_more_rooms.sum{|ad| ad.get_price_in_euro} / ads_with_four_or_more_rooms.count}"
-puts "Average price: #{total_price / total_ads}"
 
+average_price_1_room = ads_grouped[1].sum{|ad| ad.get_price_in_euro} / ads_grouped[1].count
+average_price_2_rooms = ads_grouped[2].sum{|ad| ad.get_price_in_euro} / ads_grouped[2].count
+average_price_3_rooms = ads_grouped[3].sum{|ad| ad.get_price_in_euro} / ads_grouped[3].count
+average_price_4_rooms = ads_with_four_or_more_rooms.sum{|ad| ad.get_price_in_euro} / ads_with_four_or_more_rooms.count
+
+puts "Total number of ads: #{total_ads}"
+puts "Average price: #{average_price}"
+puts "Average price one room: #{average_price_1_room}"
+puts "Average price two rooms: #{average_price_2_rooms}"
+puts "Average price three rooms: #{average_price_3_rooms}"
+puts "Average price four+ rooms: #{average_price_4_rooms}"
+
+new_row = [Date.today.to_s, total_ads, average_price, average_price_1_room, average_price_2_rooms, average_price_3_rooms, average_price_4_rooms]
+rows = CSV.read("oradea_appartments_rent.csv")
+
+Date.today.to_s == rows[-1][0] ? (rows[-1] = new_row) : rows << new_row
+
+CSV.open("oradea_appartments_rent.csv", "w") do |csv|
+  rows.each {|row| csv << row}
+end
