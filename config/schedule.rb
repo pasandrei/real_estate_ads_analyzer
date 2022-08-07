98bb65ee4988d33ec17d14ca79a9482135022715
AndreiPopovici <pasandrei98@gmail.com>
2022-08-07 19:44:08 +0300

    Added logging

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,21 +1,8 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+set :output, "log/cron.log"
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
 every 1.minute do
   puts "Test"
   # command "ruby main.rb"
   # runner "MyModel.some_method"
   # rake "some:great:rake:task"
 end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
